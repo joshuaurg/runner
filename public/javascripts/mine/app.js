cfc49e68149dc72c9ba9e8e32974ad97e556b57b
joshuaurg <352810465@qq.com>
2016-07-03 13:38:17 +0800

    update

--- public/javascripts/mine/app.js
+++ public/javascripts/mine/app.js
@@ -11,3 +11,8 @@ var serverIp = "http://123.56.8.162:8080/proz/";
 var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
+
+// Init slider and store its instance in mySwiper variable
+var mySwiper = myApp.swiper('.swiper-container', {
+    pagination:'.swiper-pagination'
+});
